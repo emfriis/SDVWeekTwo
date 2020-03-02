94554e9b54f835bf414aa0db45dd4600c9676b94
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-03-02 14:57:30 +1300

    Added index.js with code

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("Hello world");
+
+var firstName = 'Emily';
+console.log(firstName);
+
+var lastName = 'Friis';
+console.log(lastName);
+
+function printMiddleName() {;
+    var middleName = 'Margaret';
+    console.log(middleName) };
+
+printMiddleName();
+
+console.log(firstName + " " + lastName);
+
+var numFirst = 4;
+
+var numSecond = 2;
+
+var numSum = numFirst + numSecond;
+console.log(numSum);
+
+var numSub = numFirst - numSecond;
+console.log(numSub);
+
+var numDiv = numFirst / numSecond;
+console.log(numDiv);
+
+var numMult = numFirst * numSecond;
+console.log(numMult);
+
+var numRemain = numFirst % numSecond
+console.log(numRemain);
