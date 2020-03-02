5e67c7ca42609015119883920f4d31c6fc8e1d3b
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-03-03 09:26:44 +1300

    Added commenting

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-console.log("Hello world");
+console.log("Hello world"); // Prints "Hello World" to console.
 
-var firstName = 'Emily';
-console.log(firstName);
+var nameFirst = 'Emily'; // Assigns value 'Emily' to nameFirst.
+console.log(nameFirst); // Prints nameFirst value to console.
 
-var lastName = 'Friis';
-console.log(lastName);
+var nameLast = 'Friis'; // Assigns value 'Friis' to nameLast.
+console.log(nameLast); // Prints nameLast value to console.
 
-function printMiddleName() {;
-    var middleName = 'Margaret';
-    console.log(middleName) };
+function printMiddleName() {; // Function assigns value to new variable nameMiddle then prints to console.
+    var nameMiddle = 'Margaret'; // Assigns value 'Maragaret' to nameMiddle.
+    console.log(nameMiddle) }; // Prints middleName value to console.
 
-printMiddleName();
+printMiddleName(); // Calls function printMiddleName.
 
-console.log(firstName + " " + lastName);
+console.log(firstName + " " + lastName); // Prints a string containing firstName value, a space, then lastName value to the console.
 
-var numFirst = 4;
+var numFirst = 4; // Assigns value 4 to numFirst.
 
-var numSecond = 2;
+var numSecond = 2; // Assigns value 2 to numSecond.
 
-var numSum = numFirst + numSecond;
-console.log(numSum);
+var numSum = numFirst + numSecond; // Adds numFirst to numSecond, assigns that value to numSum.
+console.log(numSum);  // Prints numSum value to console.
 
-var numSub = numFirst - numSecond;
-console.log(numSub);
+var numSub = numFirst - numSecond; // Subtracts numSecond from numFirst, assigns that value to numSub.
+console.log(numSub); // Prints numSub value to console.
 
-var numDiv = numFirst / numSecond;
-console.log(numDiv);
+var numDiv = numFirst / numSecond; // Divides numFirst by numSecond, assigns that value to numDiv.
+console.log(numDiv); // Prints numDiv value to console.
 
-var numMult = numFirst * numSecond;
-console.log(numMult);
+var numMult = numFirst * numSecond; // Multiplies numFirst by numSecond, assigns that value to numMult.
+console.log(numMult); // Prints numMult value to console.
 
-var numRemain = numFirst % numSecond
-console.log(numRemain);
+var numRemain = numFirst % numSecond // Divides numFirst by Numsecond and outputs the remainder, assigns that value to numRemain.
+console.log(numRemain); // Prints numRemain value to console
